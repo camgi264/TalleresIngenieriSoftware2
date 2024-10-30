ffcd17f5fb15e96ec82ca0796cf5f4010e4279b7
camgi264 <129079924+camgi264@users.noreply.github.com>
2024-10-30 18:15:43 -0500

    Add files via upload

---
+++ Parcial/Libro.java
@@ -0,0 +1,4 @@
+//CONTROLADOR
+public class Libro {
+    
+}
---
+++ Parcial/facturaFisica.java
@@ -0,0 +1,12 @@
+//iNSIRECCIÓN
+public class Cliente {
+    private String identificacion;
+    private String nombre;
+    private String correo;
+}
+public class FacturaFisica {
+    private String codigo;
+    
+    
+    
+}
